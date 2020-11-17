663ac1681ec691f250232592ba57380e87547f6a
Samriddhi-dubey10 <74581936+Samriddhi-dubey10@users.noreply.github.com>
2020-11-17 10:19:32 +0530

    Add files via upload

---
+++ even odd/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int no;
+    cout<<"enter any no:"<<"\n";
+    cin>>no;
+    if(no % 2 == 0)
+    {
+        cout<<"even"<<"\n";
+    }
+    else
+    {
+        cout<<"odd"<<"\n";
+    }
+    return 0;
+}
